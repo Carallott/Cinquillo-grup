85aa3cbc033d72135ce358f158b9be6bbe8d5184
Carallott <hugopalaublanco@gmail.com>
2024-12-14 17:54:56 +0100

    Update pregunta_si_no.c

--- pregunta_si_no.c
+++ pregunta_si_no.c
@@ -1,1 +1,12 @@
+<stdio.h>
+
+
+int pregunta_si_no(char pregunta){
+  char pregunta;
+  scanf("%c", &pregunta);
+  if(pregunta == 's' || pregunta == 'S'){
+      return 1;
+  }
+  else{
+      return 0;
 
