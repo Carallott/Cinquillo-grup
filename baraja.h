547109de674d87fc04bc5827c44320f6c44178fb
Carallott <hugopalaublanco@gmail.com>
2024-12-14 18:31:25 +0100

    Update baraja.h

--- baraja.h
+++ baraja.h
@@ -1,1 +1,13 @@
+#include <stdio.h>
+
+#define NUM  10
+#define PALO  4
+#define CARTAS  (NUM*PALO) //definido de esta manera para que se pueda cambiar sin tocar el còdigo
+#define OROS  0
+#define ESPADAS  1
+#define BASTOS  2
+#define COPAS  3
+
+typedef struct(){
+
 
