38b6585b179f94cf691d78d082935013a764d785
alvarogc605 <alvaro.pablo.gc@gmail.com>
2024-12-14 17:04:22 +0100

    Update jugadores.h

--- jugadores.h
+++ jugadores.h
@@ -1,1 +1,12 @@
+#ifndef JUGADORES_H
+#define JUGADORES_H
+	#define MAX 10
+	#define J 4
+	typedef struct {
+		int JUG[J][MAX];
+	} tjug;
+void imprimir_jugador (int n) {
+	printf("JUG#%d:  ", n);
+	}
+#endif
 
