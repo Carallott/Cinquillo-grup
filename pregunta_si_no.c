2609984b210099951737e59ba13ecb36edd8b5ab
Carallott <hugopalaublanco@gmail.com>
2024-12-14 20:04:39 +0100

    Update pregunta_si_no.c

--- pregunta_si_no.c
+++ pregunta_si_no.c
@@ -1,14 +1,33 @@
-<stdio.h>
+#include <stdio.h>
 
+// Función que realiza la pregunta y devuelve 1 para 'sí' y 0 para 'no'
+int preguntar_si_o_no() {
+    char respuesta;
 
-int pregunta_si_no(char pregunta){
-  char pregunta;
-  scanf("%c", &pregunta);
-  if(pregunta == 's' || pregunta == 'S'){
-      return 1;
-  }
-  else if(pregunta == 'n' || pregunta == 'N'){
-      return 0;
-  }
+    printf("¿Quieres continuar? (s/n): ");
+    scanf(" %c", &respuesta);  // Leer la respuesta
+
+    if (respuesta == 's' || respuesta == 'S') {
+        return 1;  // Sí
+    } else if (respuesta == 'n' || respuesta == 'N') {
+        return 0;  // No
+    } else {
+        return -1;  // Respuesta no válida
+    }
 }
+
+int main() {
+    int respuesta = preguntar_si_o_no();  // Llamar a la función
+
+    if (respuesta == 1) {
+        printf("Has elegido continuar.\n");
+    } else if (respuesta == 0) {
+        printf("Has elegido salir.\n");
+    } else {
+        printf("Respuesta no válida.\n");
+    }
+
+    return 0;
+}
+
 
