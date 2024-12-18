6b8a599e6567235bf065e8a0ac1cbe433bc69656
alvarogc605 <alvaro.pablo.gc@gmail.com>
2024-12-18 23:20:03 +0100

    Update nueva_carta.c

--- nueva_carta.c
+++ nueva_carta.c
@@ -3,21 +3,14 @@ typedef struct {
   int jug;
   char car; //[(J)ugable (T)ablero (M)ano]
 } t_carac;
-typedef struct {
-  t_carac carac;
-  int carta; //inicialiazar carta[1]=1 y progresivamente
-} t_carta;
-t_carta c[MAX];
+t_carac c[MAX];
 void iniciar_cartas(t_carta *p_c) {
 	int i;
 	char j,m;
 	for(i=0;i<MAX;i++) {
-		p_c[i].carta=i;
-		if (p_c[i].carta==4 || p_c[i].carta==14 || p_c[i].carta==24 || p_c[i].carta==34) {
-		p_c[i].carac.car='J';
-		} else p_c[i].carac.car='M';
-		}
-}	  
+		p_c[i].carac.car='M';
+}
+}
 void asignar_cartas (t_carta *p_c) {
 	int cont[4]={0};
 	int i,x;
@@ -26,7 +19,7 @@ void asignar_cartas (t_carta *p_c) {
 		x=numero_al_azar(4);
 		cont[x]++;
 		if (cont[x]<=10) {
-		p_c[i].carac.jug=x;
+		p_c[i].jug=x;
 		
 	}	else i=i -1;
 	}
