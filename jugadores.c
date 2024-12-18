3c44709de3fb3412558dbbaa0e2eb07d909775b0
alvarogc605 <alvaro.pablo.gc@gmail.com>
2024-12-18 16:37:01 +0100

    Update jugadores.c

--- jugadores.c
+++ jugadores.c
@@ -1,11 +1,14 @@
 #include "JUGADORES_H"
-typrdef tjug JUG
-void asignar_carta() {
-    int i,j,n=41,m;
-    for(j=0;j<J;j++) {
-    for(i=0;i<CARTAS;i++) {
-         m=numero_al_azar(n);
-      JUG.JUG[j][i]=baraja[m];
-    }
-    }
-}
+void repartir_cartas(t_carta *p_c, t_jug *p_jug) {
+		int i,x,j;		
+		for(x=0;x<JUG;x++) {
+			j=0;			
+			for(i=0;i<MAX;i++) {
+				if (p_c[i].carac.jug==x) {
+					p_jug->J[x][j]=p_c[i].carta;
+					j++;
+					}
+				}
+			}
+		}
+
