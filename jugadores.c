7c435101c8ff120056603db8ae1ee0a600fb52fc
alvarogc605 <alvaro.pablo.gc@gmail.com>
2024-12-19 10:49:50 +0100

    Update jugadores.c

--- jugadores.c
+++ jugadores.c
@@ -1,14 +1,4 @@
 #include "JUGADORES_H"
-void repartir_cartas(t_carta *p_c, t_jug *p_jug) {
-		int i,x,j;		
-		for(x=0;x<JUG;x++) {
-			j=0;			
-			for(i=0;i<MAX;i++) {
-				if (p_c[i].carac.jug==x) {
-					p_jug->J[x][j]=p_c[i].carta;
-					j++;
-					}
-				}
-			}
-		}
+// hacer algoritmo que piense 
+		
 
