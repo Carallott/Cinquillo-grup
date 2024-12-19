30e50beb9039e9b0161366cb30bdccc3cc70e7a2
alvarogc605 <alvaro.pablo.gc@gmail.com>
2024-12-19 11:02:09 +0100

    Update nueva_carta.c

--- nueva_carta.c
+++ nueva_carta.c
@@ -4,14 +4,14 @@ typedef struct {
   char car; //[(J)ugable(esta en la mesa y se puedejugar con ella) (T)ablero (M)ano]
 } t_carac;
 t_carac c[MAX];
-void iniciar_cartas(t_carta *p_c) {
+void iniciar_cartas(t_carac *p_c) {
 	int i;
 	char M;
 	for(i=0;i<MAX;i++) {
 		p_c[i].carac.car='M';
 }
 }
-void asignar_cartas (t_carta *p_c) {
+void asignar_cartas (t_carac *p_c) {
 	int cont[4]={0};
 	int i,x;
 	for(i=0;i<MAX;i++) {
