e9a81401b549ad8124086d7ba9a715d4a023b384
alvarogc605 <alvaro.pablo.gc@gmail.com>
2024-12-21 13:54:31 +0100

    Update jugadores.c

--- jugadores.c
+++ jugadores.c
@@ -1,4 +1,28 @@
 #include "JUGADORES_H"
-// hacer algoritmo que piense 
+int pensar (t_jugada *p_p,t_carac *p_c, int i) { //hacer caso no hay jugada dispo
+	int x,n,y;
+	if (p_p[2].val== -1) {
+		return 1;
+		}
+	else {
+	      for(n=0;n<MAX;n++) {
+		if(p_c[n].jug==i) {
+			if(n==9||n==19||n==29||n==39) {
+				for(x=0;x<MAX_P;x++) {
+					y=n-p_p[x].val;
+					if(y<=5 && y>=0) return x;}}
+			if(n==0||n==10||n==20||n==30) {
+				for(x=0;x<MAX_P;x++) {
+					y=n-p_p[x].val;
+					if(y<=0 && y>-5) return x;}}
+			if(n==8||n==18||n==28||n==38) {
+				for(x=0;x<MAX_P;x++) {
+					y=n-p_p[x].val;
+					if(y>=0 && y<5) return x;}}
+	}		
+	}				
+	}		
+}	
+
 		
 
